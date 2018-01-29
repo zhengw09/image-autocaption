9c37337eb4af174cbcf501dc26635d62e246c254
Zheng <zhengzw@umich.edu>
2018-01-28 19:57:37 -0500

    basic html

---
+++ controllers/main.py
@@ -0,0 +1,51 @@
+from flask import *
+from werkzeug.utils import secure_filename
+import os
+# from extensions import connect_to_database
+
+main = Blueprint('main', __name__, template_folder='templates')
+
+
+def allowed_file(filename):
+    allowed_extensions = set(['png', 'jpg', 'bmp', 'gif'])
+    return '.' in filename and \
+           filename.split('.')[-1].lower() in allowed_extensions
+
+
+@main.route('/', methods=['GET', 'POST'])
+def main_route():
+    options = {}
+    # username = request.args.get('username')
+    # db = connect_to_database()
+    # cur = db.cursor()
+    # # get the users
+    # query = 'select username from User;'
+    # cur.execute(query)
+    # requests = cur.fetchall()
+    # user_list = []
+    # for result in requests:
+    #     user_list.append(result['username'])
+    # options['user_list'] = user_list
+    
+    if request.method == 'POST':
+        form = request.form
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('static/images', filename))
+            options['post'] = True
+            options['filename'] = filename
+            return render_template("index.html", **options)
+
+    else:  # 'GET'
+        options['post'] = False
+        return render_template("index.html", **options)
---
+++ utils/config.py
@@ -0,0 +1,8 @@
+# config
+env = dict(
+	host = '0.0.0.0',
+	port = 3000,
+	user = 'root', 
+	password = 'root',
+	db = 'test_db',
+)
